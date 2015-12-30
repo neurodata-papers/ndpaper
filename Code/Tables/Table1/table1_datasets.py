bb8f0c21767a970e3d0dbe9b45540fce2942a9ae
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-30 11:08:00 -0500

    move cache to Data/

---
+++ Code/Tables/Table1/table1_datasets.py
@@ -0,0 +1,50 @@
+import PyTablePrinter
+import glob
+
+"""
+This is what we're generating:
+
+Modality | Species | Dataset | Token | XYZT Resolution | Total Size | Reference |
+...
+"""
+
+path = "../../Data/metadata"
+
+# public_datasets_and_tokens = oo.get_public_datasets_and_tokens()
+public_tokens = glob.glob('{}/projinfo_cache/*.json'.format(path))
+
+#
+# tokens = oo.get_public_tokens()
+#
+# data = []
+#
+# for t in tokens:
+#     info = oo.get_token_info(t)
+#     metadata = ndl.get_metadata(t)
+#     if 'reference' in metadata:
+#         link = metadata['reference']
+#         link = "[{}]({})".format(link[11:40] + "...", link)
+#     else:
+#         link = ""
+#     data.append({
+#         "token": t,
+#         "modality": metadata['type'] if 'type' in metadata else "",
+#         "species": metadata['species'] if 'species' in metadata else "",
+#         "dataset": info['dataset']['description'],
+#         "resolution": metadata['resolution'] if 'resolution' in metadata else "",
+#         "image_size": ' x '.join([str(d) for d in info['dataset']['imagesize']['0']]),
+#         "reference": link
+#     })
+#
+# col_order = [
+#     "token",
+#     "modality",
+#     "species",
+#     "dataset",
+#     "resolution",
+#     "image_size",
+#     "reference"
+# ]
+#
+# tp = PyTablePrinter.TablePrinter(data, col_order=col_order)
+# print tp.to_markdown()
