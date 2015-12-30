0c4bc78b2d20928866d2b2d7d6142b7527f7305a
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-30 11:40:14 -0500

    add new v of table1

--- Code/Tables/Table1/table1_datasets.py
+++ Code/Tables/Table1/table1_datasets.py
@@ -19,44 +19,43 @@ token_info = []
 
 for path in public_token_files:
     with open(path, 'r') as infile:
-        token_info.append(json.loads(infile.read()))
+        info = json.loads(infile.read())
+        if 'image' not in info['channels'].keys():
+            continue
+        metadata = info['metadata']
+        if 'reference' in metadata:
+            link = metadata['reference']
+            link = "[{}]({})".format(link[11:40] + "...", link)
+        else:
+            link = ""
+        insert = {
+            "modality": metadata['type'] if 'type' in metadata else "",
+            "species": metadata['species'] if 'species' in metadata else "",
+            "dataset": "`{}`".format(info['dataset']['description']),
+            "token/channel": "`{}/image`".format(path[len(cache_path)+1:-5]),
+            "resolution": metadata['resolution'] if 'resolution' in metadata else "",
+            "image_size": ' x '.join([str(d) for d in info['dataset']['imagesize']['0']]),
+            "reference": link
 
-pt = TablePrinter(token_info)
+        }
+        token_info.append(insert)
+
+pt = TablePrinter(token_info, col_order=['modality', 'species', 'dataset', 'token/channel', 'resolution', 'image_size', 'reference'])
 print pt.to_markdown()
 
-
 #
-# tokens = oo.get_public_tokens()
-#
-# data = []
-#
-# for t in tokens:
-#     info = oo.get_token_info(t)
-#     metadata = ndl.get_metadata(t)
-#     if 'reference' in metadata:
-#         link = metadata['reference']
-#         link = "[{}]({})".format(link[11:40] + "...", link)
-#     else:
-#         link = ""
-#     data.append({
-#         "token": t,
-#         "modality": metadata['type'] if 'type' in metadata else "",
-#         "species": metadata['species'] if 'species' in metadata else "",
-#         "dataset": info['dataset']['description'],
-#         "resolution": metadata['resolution'] if 'resolution' in metadata else "",
-#         "image_size": ' x '.join([str(d) for d in info['dataset']['imagesize']['0']]),
-#         "reference": link
-#     })
-#
-# col_order = [
-#     "token",
-#     "modality",
-#     "species",
-#     "dataset",
-#     "resolution",
-#     "image_size",
-#     "reference"
-# ]
-#
-# tp = PyTablePrinter.TablePrinter(data, col_order=col_order)
-# print tp.to_markdown()
+# info = oo.get_token_info(t)
+# metadata = ndl.get_metadata(t)
+# if 'reference' in metadata:
+#     link = metadata['reference']
+#     link = "[{}]({})".format(link[11:40] + "...", link)
+# else:
+#     link = ""
+# data.append({
+#     "token": t,
+#     "modality": metadata['type'] if 'type' in metadata else "",
+#     "species": metadata['species'] if 'species' in metadata else "",
+#     "dataset": info['dataset']['description'],
+#     "resolution": metadata['resolution'] if 'resolution' in metadata else "",
+#     "image_size": ' x '.join([str(d) for d in info['dataset']['imagesize']['0']]),
+#     "reference": link
---
+++ Results/Tables/Table1/table1_v3.md
@@ -0,0 +1,21 @@
+| modality|species|dataset|token/channel|resolution|image_size|reference |
+| -----|-----|-----|-----|-----|-----|----- |
+| ||`acardona_0111_8`|`acardona_0111_8/image`||32768 x 32768 x 4840| |
+| ||`acardona_abd1_5`|`acardona_abd1_5/image`||24576 x 24576 x 461| |
+| ||``|`ara_test/image`||456 x 320 x 528| |
+| EM|Mouse|`bock11`|`bock11/image`|4 4 45 nm|135424 x 119808 x 4156|[nature.com/nature/journal/v47...](http://www.nature.com/nature/journal/v471/n7337/full/nature09802.html) |
+| ||`kasthuri11cc`|`cv_ac3_membrane_2014/image`||21504 x 26624 x 1850| |
+| ||`kasthuri11cc`|`cv_kasthuri11_membrane_2014/image`||21504 x 26624 x 1850| |
+| ||`flycol`|`flycol/image`||2000 x 2000 x 6240| |
+| EM|Mouse|`kasthuri11`|`kasthuri11/image`|3 3 30 nm|21504 x 26624 x 1850|[sciencedirect.com/science/art...](http://www.sciencedirect.com/science/article/pii/S0092867415008247) |
+| ||`kasthuri14Maine`|`kasthuri14Maine/image`||6144 x 6144 x 2042| |
+| ||`kasthuri14s1colEM`|`kasthuri14s1colEM/image`||49152 x 32768 x 254| |
+| ||`kharris15apical`|`kharris15apical/image`||8192 x 8192 x 194| |
+| ||`kharris15oblique`|`kharris15oblique/image`||8192 x 8192 x 91| |
+| ||`kharris15spine`|`kharris15spine/image`||9000 x 9000 x 89| |
+| ||`The MNI152 template atlas space`|`mniatlas/image`||182 x 218 x 182| |
+| ||`kasthuri11`|`ndio_demos/image`||21504 x 26624 x 1850| |
+| EM|Worm|`ritaN2`|`ritaN2/image`|? ? 50 nm|13101 x 14378 x 1199|[wormatlas.org/ver1/MoW_built0...](http://www.wormatlas.org/ver1/MoW_built0.92/toc.html) |
+| EM|Worm|`ritaN2_5`|`ritaN2_5/image`|? ? 50 nm|37379 x 25986 x 743|[wormatlas.org/ver1/MoW_built0...](http://www.wormatlas.org/ver1/MoW_built0.92/toc.html) |
+| EM|Worm|`ritaN2_four`|`ritaN2_four/image`|? ? 50 nm|28381 x 24234 x 322|[wormatlas.org/ver1/MoW_built0...](http://www.wormatlas.org/ver1/MoW_built0.92/toc.html) |
+| EM|Drosophila|`takemura13`|`takemura13/image`||12000 x 12000 x 1460|[nature.com/nature/journal/v50...](http://www.nature.com/nature/journal/v500/n7461/full/nature12450.html#/semi-automated-connectome-reconstruction) |
